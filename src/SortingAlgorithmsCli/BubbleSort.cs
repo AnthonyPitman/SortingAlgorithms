70ff7ceab89a33318dc4ce0b4eb34e36997d2161
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2025-05-31 13:47:40 -0400

    add basic bubble sort

---
+++ src/SortingAlgorithmsCli/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿namespace SortingAlgorithmsCli;
+
+public static class BubbleSort
+{
+    public static void Sort(int[] arr)
+    {
+        var upper = arr.GetUpperBound(0);
+        for (var outer = upper; outer >= 1; outer--)
+        {
+            for (var inner = 0; inner <= outer - 1; inner++)
+            {
+                if (arr[inner] > arr[inner + 1])
+                {
+                    Utils.Swap(arr, inner, inner + 1);
+                }
+            }
+        }
+    }
+}
+
+public static class Utils
+{
+    public static void Swap(int[] arr, int left, int right)
+    {
+        (arr[left], arr[right]) = (arr[right], arr[left]);
+    }
+}
---
+++ src/SortingAlgorithmsCli/Program.cs
@@ -0,0 +1,10 @@
+﻿using SortingAlgorithmsCli;
+
+int[] arr = [1, 2, 3, 9, 8, 7, 6, 5, 0, 4];
+Console.WriteLine("Before sorting");
+Console.WriteLine(string.Join(' ', arr));
+
+BubbleSort.Sort(arr);
+
+Console.WriteLine("After sorting");
+Console.WriteLine(string.Join(' ', arr));
